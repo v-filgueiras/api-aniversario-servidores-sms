de4b07e5b548f1bc3afc53991aecfcfb1fbbd62a
v-filgueiras <vfilgueiras.dev@gmail.com>
2026-05-26 11:31:22 -0400

    updating structure

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-from fastapi import FastAPI
+import os
 import uvicorn
+from fastapi import FastAPI
 
 from routes.employees import router
-
-from database.connect import (
-    Base,
-    engine
-)
-
-from services.scheduler_service import (
-    start_scheduler
-)
-
+from database.connect import Base, engine
+from services.scheduler_service import start_scheduler
 
 app = FastAPI()
 
@@ -22,15 +15,12 @@ Base.metadata.create_all(bind=engine)
 
 @app.on_event("startup")
 async def startup_event():
-
     start_scheduler()
 
 
 if __name__ == "__main__":
-
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=3001,
-        reload=True
+        port=int(os.environ.get("PORT", 8000))
     )
