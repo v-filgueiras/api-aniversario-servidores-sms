c3958b1807adc67a2980f8f9fccd6eb2c44ee445
v-filgueiras <vfilgueiras.dev@gmail.com>
2026-08-24 08:19:56 -0400

    arrumando a string de conexão

--- database/connect.py
+++ database/connect.py
@@ -1,20 +1,15 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-
-DATABASE_URL = (
-    "postgresql://postgres.rvfwutfabnfcgwhdylqo:emeOw8aXIFB7tx79@aws-1-sa-east-1.pooler.supabase.com:6543/postgres?sslmode=require"
-)
+# Adicionado .rvfwutfabnfcgwhdylqo no usuario para autenticar no Supavisor
+DATABASE_URL = "postgresql://postgres.rvfwutfabnfcgwhdylqo:emeOw8aXIFB7tx79@aws-1-sa-east-1.pooler.supabase.com:6543/postgres?sslmode=require"
 
 engine = create_engine(
-    DATABASE_URL
+    DATABASE_URL,
+    pool_pre_ping=True,  # Testa a conexão antes de usar para evitar desconexões
 )
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
